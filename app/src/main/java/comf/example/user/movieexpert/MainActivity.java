5c5847d76e9453bf00a6cb1ba778824c59690289
julianmosi7 <moshammer.ju@gmail.com>
2019-04-05 20:30:37 +0200

    second commit

--- app/src/main/java/comf/example/user/movieexpert/MainActivity.java
+++ app/src/main/java/comf/example/user/movieexpert/MainActivity.java
@@ -1,13 +1,47 @@
 package comf.example.user.movieexpert;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Movie> movies = new ArrayList();
+    public String FILENAME = "movies.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private InputStream getInputStreamForAsset(String filename){
+        AssetManager assets = getAssets();
+        try{
+            return assets.open(filename);
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void readAssets(){
+        InputStream in = getInputStreamForAsset(FILENAME);
+        BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+        String line;
+        try{
+            while((line = bin.readLine()) != null){
+
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/comf/example/user/movieexpert/Movie.java
@@ -0,0 +1,11 @@
+package comf.example.user.movieexpert;
+
+import java.util.Date;
+
+public class Movie {
+    String title;
+    double rating;
+    Date release_date;
+    String description;
+
+}
